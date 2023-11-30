a6a9f3386af0525e77251c0413505039ff23ad65
zerotower69 <zerotower@163.com>
2023-11-30 23:33:58 +0800

    环境变量

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+//use unocss
+import 'virtual:uno.css';
+
+const app = createApp(App);
+app.mount('#app');
