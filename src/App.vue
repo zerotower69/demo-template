331c05a97c2653a5ab2c3b356e0a5c6e3d777500
zerotower69 <zerotower@163.com>
2023-11-30 23:29:52 +0800

    vue3 初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>vue3 快速demo搭建模板</p>
+  </div>
+</template>
+
+<style scoped>
+div{
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+p{
+  font-size: 40px;
+}
+</style>
